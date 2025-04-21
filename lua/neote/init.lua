0c6f6b57428de33a34f486fbee3e6bfcce677741
Apricot001024 <tsinhood@gmail.com>
2025-04-21 22:29:21 +0800

    graph: re do

--- lua/neote/init.lua
+++ lua/neote/init.lua
@@ -3,12 +3,13 @@ local M = {}
 M.config = {
     notes_dir = vim.fn.expand("~/notes"),
     templates_dir = vim.fn.expand("~/notes/templates"),
+    assets_dir = vim.fn.expand(vim.fn.stdpath("data").."/neote.nvim/assets"),
 }
 
 function M.setup(opts)
     M.config = vim.tbl_deep_extend("force", M.config, opts or {})
     -- 自动创建目录
-    for _, dir in ipairs({M.config.notes_dir, M.config.templates_dir}) do
+    for _, dir in ipairs({M.config.notes_dir, M.config.templates_dir, M.config.assets_dir}) do
         if vim.fn.isdirectory(dir) == 0 then
             vim.fn.mkdir(dir, "p")
         end
@@ -21,6 +22,7 @@ function M.setup(opts)
     vim.api.nvim_create_user_command("NeoteCapture", function(opts)
         require("neote.capturenote").create(opts.args)
     end, {nargs = "?"})
+    -- require("neote.graph").setup()
 end
 
 return M
